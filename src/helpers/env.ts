36bcccb01fe276adcd83126a63f5a62c88c74f49
Philip Oesterlin <philip.oesterlin@gmail.com>
2021-11-18 21:35:43 +0100

    change endpoint

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,3 +1,3 @@
 
 const env = import.meta.env;
-export const server = env.VITE_SERVER ?? `https://hzfrvzijxi.execute-api.eu-central-1.amazonaws.com/production`;
+export const server = env.VITE_SERVER ?? `https://m0mc1vh5jj.execute-api.eu-central-1.amazonaws.com/production`;
