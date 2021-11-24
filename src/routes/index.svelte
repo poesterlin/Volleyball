a1e1968e4daa16551254b1229aa5dddeaae2fa96
Philip Oesterlin <philip.oesterlin@gmail.com>
2021-11-24 15:49:53 +0100

    improve registration ux

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,7 +18,7 @@
 
 	async function update() {
 		loading = true;
-		const res = await fetch(server + '/courses').then(r => r.json());
+		const res = await fetch(server + '/courses').then((r) => r.json());
 		courses = res.courses;
 		loading = false;
 	}
@@ -41,7 +41,7 @@
 				Accept: 'application/json',
 				'Content-Type': 'application/json'
 			}
-		}).then(j => j.json());
+		}).then((j) => j.json());
 
 		localStorage.setItem('lastKey', res.registration.key);
 		registration = res.registration;
@@ -66,11 +66,68 @@
 			return;
 		}
 
-		const { name, time, date } = courses.find(c => c._id === courseID);
+		const { name, time, date } = courses.find((c) => c._id === courseID);
 
 		return `${name} course on ${humanReadableDate(date)} at ${time}`;
 	}
 </script>
+
+{#if loading}
+	<div id="overlay">
+		<div class="lds-dual-ring" />
+	</div>
+{/if}
+<h1>🏐 Register for Volleyball 🏐</h1>
+<div id="links">
+	<a href="/check" sveltekit:prefetch>Check Registration</a>
+	<a href="/login">Admin</a>
+	<a href="https://github.com/poesterlin/Volleyball">Contribute</a>
+</div>
+<main>
+	<span>Please put in your name and select a course to attend.</span>
+	<input type="text" placeholder="Name" on:keyup={isEnter} name="name" bind:value={name} />
+	<div id="list">
+		{#each courses as course}
+			<Course
+				{course}
+				on:select={(c) => (courseID = c.detail.course)}
+				selected={course._id === courseID}
+			/>
+		{/each}
+
+		{#if courses.length === 0}There are currently no courses. ☹{/if}
+	</div>
+	{#if !canSend && triedToSend}
+		<label for="name">Name too short!</label>
+	{/if}
+	<button id="register" on:click={send} class:disabled={!canSend}>Register</button>
+	{#if registration && showOverlay}
+		<div id="overlay" on:click={() => (showOverlay = false)}>
+			<div id="inner">
+				{#if registration.registeredTwice}
+					<span class="underline">You are already registered for this course.</span>
+				{:else}
+					<h3>
+						You registered for:
+						<span class="underline">{getCourseInfo()}</span>
+					</h3>
+				{/if}
+				<p>
+					{#if registration.waitlist}
+						<b class="red">You are on the waitlist. Check your registration status again later.</b><br><br>
+						<span>If you want to, you can register for E-Mail updates below. </span> <br><br>
+					{/if}
+					Registration Code:
+					<b>{registration.key}</b>
+				</p>
+				<div id="actions">
+					<button on:click={() => copy(registration.key)}>Copy Registration Code</button>
+					<a id="check" href="/check?key={registration.key}">Check Registration</a>
+				</div>
+			</div>
+		</div>
+	{/if}
+</main>
 
 <style type="text/scss">
 	$padding: 5px;
@@ -109,7 +166,17 @@
 		border: 1px solid gray;
 		padding: 2%;
 	}
-	button {
+
+	#actions {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		flex-wrap: wrap;
+		gap: 10px;
+	}
+	button,
+	a#check {
 		display: block;
 		margin: 15px auto;
 		width: 100%;
@@ -120,6 +187,20 @@
 		border-radius: 15px;
 		font-weight: bold;
 		cursor: pointer;
+	}
+
+	button:not(#register) {
+		background: white;
+		color: black;
+		border: 5px solid rgba(0, 0, 0, 0.267);
+	}
+	button:not(#register),
+	a#check {
+		flex: 1 1 calc(50% - 5px);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 16px;
 	}
 
 	#links {
@@ -200,55 +281,3 @@
 		}
 	}
 </style>
-
-{#if loading}
-	<div id="overlay">
-		<div class="lds-dual-ring" />
-	</div>
-{/if}
-<h1>🏐 Register for Volleyball 🏐</h1>
-<div id="links">
-	<a href="/check" sveltekit:prefetch>Check Registration</a>
-	<a href="/login">Admin</a>
-	<a href="https://github.com/poesterlin/Volleyball">Contribute</a>
-</div>
-<main>
-	<span>Please put in your name and select a course to attend.</span>
-	<input type="text" placeholder="Name" on:keyup={isEnter} name="name" bind:value={name} />
-	<div id="list">
-		{#each courses as course}
-			<Course
-				{course}
-				on:select={c => (courseID = c.detail.course)}
-				selected={course._id === courseID} />
-		{/each}
-
-		{#if courses.length === 0}There are currently no courses. ☹{/if}
-	</div>
-	{#if !canSend && triedToSend}
-		<label for="name">Name too short!</label>
-	{/if}
-	<button on:click={send} class:disabled={!canSend}>Register</button>
-	{#if registration && showOverlay}
-		<div id="overlay" on:click={() => (showOverlay = false)}>
-			<div id="inner">
-				{#if registration.registeredTwice}
-					<span class="underline">You are already registered for this course.</span>
-				{:else}
-					<h3>
-						You registered for:
-						<span class="underline">{getCourseInfo()}</span>
-					</h3>
-				{/if}
-				<p>
-					{#if registration.waitlist}
-						<b class="red">You are on the waitlist. Check your registration status again later.</b>
-					{/if}
-					Registration Code:
-					<b>{registration.key}</b>
-				</p>
-				<button on:click={() => copy(registration.key)}>Copy Registration Code</button>
-			</div>
-		</div>
-	{/if}
-</main>
