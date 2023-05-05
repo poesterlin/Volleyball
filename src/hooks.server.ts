f47e61b943933d83e6901d578e68ec63ad85fe0a
Philip Oesterlin <oesterlin@ugd.ai>
2023-05-05 13:29:11 +0200

    log errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,14 @@
-// import { authenticateUser } from "$lib/db/auth"
-// import { redirect, type Handle } from "@sveltejs/kit"
+import type { Handle } from "@sveltejs/kit"
 
-// export const handle: Handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
+    // if (event.url.pathname.startsWith("/admin")) {
+    //     event.locals.user = authenticateUser(event);
 
-//     if (event.url.pathname.startsWith("/admin")) {
-//         event.locals.user = authenticateUser(event);
+    //     if (event.locals.user?.role !== "ADMIN") {
+    //         throw redirect(303, "/")
+    //     }
+    // }
 
-//         if (event.locals.user?.role !== "ADMIN") {
-//             throw redirect(303, "/")
-//         }
-//     }
 
-//     const response = await resolve(event)
-
-//     return response
-// }
+    return resolve(event);
+}
--- src/routes/(public)/+page.server.ts
+++ src/routes/(public)/+page.server.ts
@@ -1,21 +1,26 @@
 import { connectDB } from '$lib/db/connection';
 import { DB } from '$lib/db/schema';
+import { error } from '@sveltejs/kit';
 
 export const load = (async () => {
-    await connectDB();
+    try {
+        await connectDB();
 
-    const courses = await DB.Course.find({
-        publishOn: {
-            $lt: new Date()
-        }
-    }).sort({ date: 1, time: 1 });
-
-    return {
-        courses: courses.map((c) => {
-            const resp = c.toObject();
-            resp._id = c._id.toString();
-            resp.registered = resp.registered.length as any;
-            return resp;
-        })
-    };
-}) 
+        const courses = await DB.Course.find({
+            publishOn: {
+                $lt: new Date()
+            }
+        }).sort({ date: 1, time: 1 });
+        return {
+            courses: courses.map((c) => {
+                const resp = c.toObject();
+                resp._id = c._id.toString();
+                resp.registered = resp.registered.length as any;
+                return resp;
+            })
+        };
+    } catch (err: any) {
+        console.error(err);
+        throw error(500, err.toString())
+    }
+});
