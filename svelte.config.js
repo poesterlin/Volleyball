d200d62dd2ef85b7c0666740e6fd7013cdbcb133
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2023-11-16 18:06:32 +0100

    use edge function

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +9,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			edge: false,
-			split: true
+			edge: true,
+			split: false
 		})
 	}
 };
