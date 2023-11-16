bf59a9f9451fd10e2f3d580caf8b0d1286f165e2
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2023-11-16 18:03:14 +0100

    eol iframe

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<iframe src="https://volleyball.oesterlin.dev" frameborder="0" />
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+
+	iframe {
+		width: 100%;
+		height: 100dvh;
+		border: 0;
+	}
+</style>
